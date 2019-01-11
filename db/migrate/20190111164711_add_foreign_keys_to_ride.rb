f6e11b05d435c30775eb40ee240babda804bc92b
Melissa Real <melissa.real@hotmail.com>
2019-01-11 16:56:20 +0000

    Automatically backed up by Learn

--- db/migrate/20190111164711_add_foreign_keys_to_ride.rb
+++ db/migrate/20190111164711_add_foreign_keys_to_ride.rb
@@ -1,4 +1,4 @@
-class AddForeignKeysToRid < ActiveRecord::Migration
+class AddKeysToRide < ActiveRecord::Migration
   def change
       add_column :rides, :passenger_id, :integer
       add_column :rides, :taxi_id, :integer
